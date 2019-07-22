9df9481456a0b2a732b6ecc96c9fea9972c86199
qilobit <qilobit@gmail.com>
2019-07-22 01:00:54 -0400

    adding login

--- src/router.js
+++ src/router.js
@@ -4,35 +4,78 @@ import Companies from './components/CompaniesList.vue'
 import Products from './components/Products.vue'
 import Orders from './components/Orders.vue'
 import Reports from './components/Reports.vue'
+import Login from './components/Login.vue'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
-      path: '/companies',
+      path: '/',
       name: 'home',
-      component: Companies
+      component: Companies,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/companies',
       name: 'companies',
-      component: Companies
+      component: Companies,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/products/',
       name: 'products',
-      component: Products
+      component: Products,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: '/orders',
       name: 'orders',
-      component: Orders
+      component: Orders,
+      meta: {
+        requiresAuth: false
+      }
     },
     {
       path: '/reports',
       name: 'reports',
-      component: Reports
+      component: Reports,
+      meta: {
+        requiresAuth: false
+      }
     }     
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    if(!firebase.auth().currentUser){
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    }else{
+      next();
+    }
+  }else{
+    next();
+  }
+
+});
+
+ export default router
